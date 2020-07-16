da7592dd973da5e97a49199cae84a981a179ae5b
luanslf <fernaul19>
2020-07-16 12:24:46 -0300

    scrape na tabela de casos por bairro no site caxiascovid19

--- scrape.js
+++ scrape.js
@@ -3,13 +3,37 @@ const puppeteer = require('puppeteer');
 const scrape = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://books.toscrape.com/');
+    await page.goto('https://caxiascovid19.com.br/');
+    //await page.goto('http://books.toscrape.com/');
 
     const result = await page.evaluate(() => {
-        const books = []
+        const districts = [];
+        document.querySelectorAll('main#main > section#team > div.container > div.row > div.table-responsive > table.table.table-striped > tbody tr')
+            .forEach((tr) => {
+                const district = {};
+                const trChildren = tr.children;
+                district['district'] = trChildren[0].innerText;
+                district['confirmed'] = trChildren[1].innerText;
+                district['recovered'] = trChildren[2].innerText;
+                /* for (i = 0; i < trChildren.length; i++) {
+                    district['district'] = trChildren[0]; */
+
+                    /* district.push(trChildren[i].innerText); */
+                    /* if (i === 0) {
+                        district['district'] = trChildren[i]
+                    } else if (i === 1) {
+
+                    } else {
+
+                    } */
+                //}
+                districts.push(district);
+            });
+        return districts;
+        /* const books = [];
         document.querySelectorAll('section > div > ol > li img')
             .forEach(book => books.push(book.getAttribute('alt')));
-        return books;
+        return books; */
     })
 
     browser.close();
