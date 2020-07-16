1cd9b60fc9b9405bf6e2643fb69910660bc8a581
luanslf <fernaul19>
2020-07-16 00:11:35 -0300

    captura de tela

--- screenshot.js
+++ screenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 async function takeScreenshot() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://caxiascovid19.com.br/');
+    await page.goto('http://books.toscrape.com/');
     await page.screenshot({path: 'screenshot.png'});
     await browser.close();
 }
